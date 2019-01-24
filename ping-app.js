926494ed47648a33146894e1669fde1601545079
Lari Korpi <lari.korpi@tieto.com>
2019-01-24 14:10:59 +0200

    imported the exsiting code

---
+++ ping-app.js
@@ -0,0 +1,18 @@
+'usr strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.response('<html><head><title>Ping App</title></head><body><h1>Success!</h1></body></html>');
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Now listening on http://${HOST}:${PORT}`);
+
